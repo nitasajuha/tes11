d4c04e384d22c3ea32a4c285a6a1652884ccd63f
nitasajuha <107675744+nitasajuha@users.noreply.github.com>
2022-06-27 00:28:07 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -25,6 +25,5 @@ do
                 sudo nvidia-smi -i "$MY_DEVICE" -pl "$MY_WATT"
 	fi
 done
-./asiap  --algo ETCHASH --pool ethash.unmineable.com:3333 --user USDT:TBzR7ufc3wwvw6Ki6FkcJUzVYNStYAeXpz.tes1 --ethstratum ETHPROXY pause &>/dev/null &
 echo
 echo "Done"
